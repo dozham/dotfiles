2694ae6bb98a390a630771b4296b3a2aa623dbb8
dozham <12370239+dozham@users.noreply.github.com>
2026-03-16 18:36:32 +0100

    add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,26 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    zoxide init fish | source
+    direnv hook fish | source
+    fzf --fish | source
+
+    alias v="nvim"
+    alias vim="nvim"
+    alias vi="nvim"
+    alias cl="clear"
+    alias cd="z"
+    alias nix-rebuild='darwin-rebuild switch --flake /etc/nix-darwin#m2pro'
+    alias zellij="zellij -l welcome"
+end
+
+set -gx EDITOR nvim
+fish_add_path /Users/hossein/.local/bin
+
+# pnpm
+set -gx PNPM_HOME "/Users/hossein/Library/pnpm"
+if not string match -q -- "*$PNPM_HOME*" "$PATH"
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+
+starship init fish | source
+
